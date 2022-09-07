faeb42744dedc890582572e8ae72a48e7f1ff336
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2022-09-07 17:24:33 -0300

    feat: implementando o cacheClient

---
+++ lib/infra/cache/local_storage_adapter.dart
@@ -0,0 +1,30 @@
+import '../../data/cache/cache.dart';
+
+import 'package:localstorage/localstorage.dart';
+
+class LocalStorageAdapter implements CacheStorage {
+  final LocalStorage localStorage;
+
+  LocalStorageAdapter({required this.localStorage});
+
+  @override
+  Future<void> save({required String key, required dynamic value}) async {
+    await localStorage.deleteItem(key);
+    await localStorage.setItem(key, value);
+  }
+
+  @override
+  Future<void> delete(String key) async {
+    await localStorage.deleteItem(key);
+  }
+
+  @override
+  Future<void> deleteAll() async {
+    await localStorage.clear();
+  }
+
+  @override
+  Future<dynamic> fetch(String key) async {
+    return await localStorage.getItem(key);
+  }
+}
---
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -0,0 +1,103 @@
+import 'package:faker/faker.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:shoppinglist/infra/cache/cache.dart';
+import 'package:test/test.dart';
+
+import '../mocks/local_storage_spy.dart';
+
+void main() {
+  late LocalStorageAdapter sut;
+  late LocalStorageSpy localStorage;
+  late String key;
+  late dynamic value;
+  late String result;
+
+  setUp(() {
+    key = faker.randomGenerator.string(5);
+    value = faker.randomGenerator.string(50);
+    result = faker.randomGenerator.string(50);
+    localStorage = LocalStorageSpy();
+    localStorage.mockFetch(result);
+    sut = LocalStorageAdapter(localStorage: localStorage);
+  });
+
+  group('@save', () {
+    test('Deve chamar o LocalStorage com os valores corretos', () async {
+      await sut.save(key: key, value: value);
+
+      verify(() => localStorage.deleteItem(key)).called(1);
+      verify(() => localStorage.setItem(key, value)).called(1);
+    });
+
+    test('Deve lançar uma exceção caso o @deleteitem quebre', () async {
+      localStorage.mockDeleteError();
+
+      final future = sut.save(key: key, value: value);
+
+      expect(future, throwsA(const TypeMatcher<Exception>()));
+    });
+
+    test('Deve lançar uma exceção caso o @deleteitem quebre', () async {
+      localStorage.mockSaveError();
+
+      final future = sut.save(key: key, value: value);
+
+      expect(future, throwsA(const TypeMatcher<Exception>()));
+    });
+  });
+
+  group('@delete', () {
+    test('Deve chamar o LocalStorage com os valores corretos', () async {
+      await sut.delete(key);
+
+      verify(() => localStorage.deleteItem(key)).called(1);
+    });
+
+    test('Deve lançar uma exceção caso o @deleteitem quebre', () async {
+      localStorage.mockDeleteError();
+
+      final future = sut.delete(key);
+
+      expect(future, throwsA(const TypeMatcher<Exception>()));
+    });
+  });
+
+  group('@deleteAll', () {
+    test('Deve chamar o LocalStorage corretamente', () async {
+      await sut.deleteAll();
+
+      verify(() => localStorage.clear()).called(1);
+    });
+
+    test('Deve lançar uma exceção caso o @clear quebre', () async {
+      localStorage.mockDeleteAllError();
+
+      final future = sut.deleteAll();
+
+      expect(future, throwsA(const TypeMatcher<Exception>()));
+    });
+  });
+
+  group('@fetch', () {
+    test('Deve chamar o LocalStorage com os valores corretos', () async {
+      await sut.fetch(key);
+
+      verify(() => localStorage.getItem(key)).called(1);
+    });
+
+    test('Deve retornar os valores corretos que estão no LocalStorage',
+        () async {
+      final data = await sut.fetch(key);
+
+      expect(data, result);
+    });
+
+    test('Deve lançar uma exceção caso o @getitem quebre', () async {
+      localStorage.mockFetchError();
+
+      final future = sut.fetch(key);
+
+      expect(future, throwsA(const TypeMatcher<Exception>()));
+    });
+  });
+}
