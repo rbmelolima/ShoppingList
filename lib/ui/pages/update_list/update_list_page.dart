96072f83fd55b491e72d1cdc5779c72e72a86e10
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2022-10-08 13:30:44 -0300

    wip: preparando os arquivos para a interface de atualizar lista

---
+++ lib/ui/pages/update_list/update_list_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UpdateListPage extends StatelessWidget {
+  const UpdateListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
