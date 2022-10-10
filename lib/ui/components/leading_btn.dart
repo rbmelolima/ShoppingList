f8eb1c974cff157685902e03a2468566ad8861d4
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2022-10-10 20:13:03 -0300

    refactor: implementando o onBack no leadingBtn

--- lib/ui/components/leading_btn.dart
+++ lib/ui/components/leading_btn.dart
@@ -2,14 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:shoppinglist/ui/style/color.dart';
 
 class LeadingBtn extends StatelessWidget {
+  final Function? onBack;
+
   const LeadingBtn({
     Key? key,
+    this.onBack,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () => Navigator.pop(context),
+      onPressed: () {
+        if (onBack == null) {
+          Navigator.pop(context);
+        } else {
+          onBack!();
+        }
+      },
       padding: const EdgeInsets.all(0),
       splashRadius: 1,
       icon: Icon(
