c0057b7cd16958f31c6c68a800bc8585c77caa92
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2022-10-25 16:27:16 -0300

    feat: adicionando o lottie

--- lib/ui/assets/assets_helper.dart
+++ lib/ui/assets/assets_helper.dart
@@ -1,5 +1,7 @@
 class Assets{
   static String illustrationPreLogin = "ilustracao-pre-login.svg";
+  static String illustrationLoadingAnalysis = "shopping_lady.json";
 }
 
 String pathOfAsset(String nameOfAsset) => "lib/ui/assets/images/$nameOfAsset";
+String pathOfLottie(String nameOfAsset) => "lib/ui/assets/lottie/$nameOfAsset";
