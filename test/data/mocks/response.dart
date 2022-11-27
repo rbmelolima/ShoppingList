a975dccb6cdac2d9012c7c373d42289ff66ef90c
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2022-11-27 20:11:50 -0300

    temp: trabalhando alguns testes

--- test/data/mocks/response.dart
+++ test/data/mocks/response.dart
@@ -1,26 +1,22 @@
-{
+const response = {
   "fornecedores": [
     {
       "nome": "Pão de Açúcar",
       "produtos": [
         {
           "nome": "Maçã Nacional Qualitá 1kg",
-          "quantidade": 1,
+          "quantidade": 1.0,
           "precoUnitario": 15.39
         }
       ],
-      "precoTotal": "R$ 15,39"
+      "precoTotal": "R\$ 15,39"
     },
     {
       "nome": "Carrefour",
       "produtos": [
-        {
-          "nome": "Maçã Pink Lady - 1kg",
-          "quantidade": 1,
-          "precoUnitario": 11.9
-        }
+        {"nome": "Maçã Pink Lady - 1kg", "quantidade": 1.0, "precoUnitario": 11.9}
       ],
-      "precoTotal": "R$ 11,90"
+      "precoTotal": "R\$ 11,90"
     }
   ],
   "fornecedorMaisCompetitivo": {
@@ -28,10 +24,10 @@
     "produtos": [
       {
         "nome": "Maçã Pré-Lavada Gala Pra Valer 1kg",
-        "quantidade": 1,
+        "quantidade": 1.0,
         "precoUnitario": 11.39
       }
     ],
-    "precoTotal": "R$ 11,39"
+    "precoTotal": "R\$ 11,39"
   }
-}
+};
